e5c1b201ea33b7fb226bde07d1dec182fd6f83de
imran1khan <107169527+imran1khan@users.noreply.github.com>
2024-04-24 20:14:33 +0530

    websoket backend

--- dist/app.js
+++ dist/app.js
@@ -1,5 +1,23 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
-const hello_1 = require("./hello");
-(0, hello_1.funhello)();
+// can you please check this code and tell me why it's not working
+const express_1 = __importDefault(require("express"));
+const ws_1 = require("ws");
+const app = (0, express_1.default)();
+const httpServer = app.listen(8080);
+const wss = new ws_1.WebSocketServer({ server: httpServer });
+wss.on('connection', function connection(ws) {
+    ws.on('error', console.error);
+    ws.on('message', function message(data, isBinary) {
+        wss.clients.forEach(function each(client) {
+            if (client.readyState === ws_1.WebSocket.OPEN) {
+                client.send(data, { binary: isBinary });
+            }
+        });
+    });
+    ws.send('Hello! Message From Server!!');
+});
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,22 @@
-import { funhello } from "./hello";
+// can you please check this code and tell me why it's not working
+import express from 'express'
+import { WebSocketServer,WebSocket } from 'ws'
 
-funhello();
+const app = express()
+const httpServer = app.listen(8080)
+
+const wss = new WebSocketServer({ server: httpServer });
+
+wss.on('connection', function connection(ws) {
+  ws.on('error', console.error);
+
+  ws.on('message', function message(data, isBinary) {
+    wss.clients.forEach(function each(client) {
+      if (client.readyState === WebSocket.OPEN) {
+        client.send(data, { binary: isBinary });
+      }
+    });
+  });
+
+  ws.send('Hello! Message From Server!!');
+});
